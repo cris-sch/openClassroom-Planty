37f747ca4bebef3b46db60934c283f5fdaaf9a83
cris-sch <schapmanc@gmail.com>
2023-10-27 16:53:39 +0200

    mise en place des image pour la page commander

--- wp-content/themes/planty/templates/template_commande.php
+++ wp-content/themes/planty/templates/template_commande.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Template commande
  */
@@ -11,49 +12,63 @@ get_header();
     <h1><?php the_title(); ?></h1>
     <section>
 
-    <?php
-    // Assurez-vous que $definitions soit défini ou récupéré avant cette boucle
-    foreach ($definitions as $definition) {
-        get_template_part("template-parts/bloc-commande", null, $definition);
-    }
-    ?>
+        <?php
+        // Assurez-vous que $definitions soit défini ou récupéré avant cette boucle
+        foreach ($definitions as $definition) {
+            get_template_part("template-parts/bloc-commande", null, $definition);
+        }
+        ?>
     </section>
- 
+
 </main><!-- #site-content -->
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Titre de votre page</title>
 </head>
+
 <body>
-    <p>Voici des images :</p>
-    <img src="../images/fraise.png">
-    <img src="../images/pample-mousse.png">
-    <img src="../images/citron.png">
-    <img src="../images/framboise.png">
-</body>
-</html>
+    <div class="parfum-container">
+        <div>
+            <img src="<?php bloginfo('url'); ?>/wp-content/themes/planty/images/Fraise.png">
+            <input id="ticketNum" type="number" name="ticketNum" value="0" />
+            <button class="command-button">OK</button>
+        </div>
+        <div>
+            <img src="<?php bloginfo('url'); ?>/wp-content/themes/planty/images/Pamplemousse.png">
+            <input id="ticketNum" type="number" name="ticketNum" value="0" />
+            <button class="command-button">OK</button>
+        </div>
+        <div>
+            <img src="<?php bloginfo('url'); ?>/wp-content/themes/planty/images/Citron.png">
+            <input id="ticketNum" type="number" name="ticketNum" value="0" />
+            <button class="command-button">OK</button>
+        </div>
+        <div>
+            <img src="<?php bloginfo('url'); ?>/wp-content/themes/planty/images/Framboise.png">
+            <input id="ticketNum" type="number" name="ticketNum" value="0" />
+            <button class="command-button">OK</button>
+        </div>
+     </div>
 
-<div class="parfum-container">
-    <label for="ticketNum">Numéro de ticket : </label>
-    <input id="ticketNum" type="number" name="ticketNum" value="0" />
-    <button class="command-button">OK</button>
-    <!-- Vous pouvez répéter le bloc ci-dessus pour chaque entrée de ticket -->
-</div>
-
-<script>
-    const mots = document.querySelectorAll("#lexique .mot");
-    mots.forEach(mot => {
-        mot.addEventListener("click", (e) => {
-            if (!mot.classList.contains("open")) {
-                mots.forEach(m => {
-                    m.classList.remove("open");
+        <script>
+            const mots = document.querySelectorAll("#lexique .mot");
+            mots.forEach(mot => {
+                mot.addEventListener("click", (e) => {
+                    if (!mot.classList.contains("open")) {
+                        mots.forEach(m => {
+                            m.classList.remove("open");
+                        });
+                        mot.classList.add("open");
+                    } else {
+                        mot.classList.remove("open");
+                    }
                 });
-                mot.classList.add("open");
-            } else {
-                mot.classList.remove("open");
-            }
-        });
-    });
-</script>
+            });
+        </script>
+
+</body>
+
+</html>
